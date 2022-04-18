c30b25aabef7ed8b3dc62b225a6fe538e52d7743
Tomohiro <tomohiroyamaoka@icloud.com>
2022-04-19 06:57:35 +0900

    コンストラクタとゲッターの追加

--- src/SimpleCalc.php
+++ src/SimpleCalc.php
@@ -1,12 +1,25 @@
 <?php
-class SimpleCalc{
-    public $number;
+class SimpleCalc
+{
+    private $number;
 
+    //インスタンス作成時に、初期化をする
+    //第一引数に初期値を設定する
+    public function __construct($number = 0)
+    {
+        $this->number = $number;
+    }
+    public function setNumber($number){
+        $this -> number = $number;
+    }
     public function add($x)
     {
         $this->number = $this->number + $x;
     }
     public function show(){
         echo $this->number;
+    }
+    public function getNumber(){
+        return $this -> number;
     }
 }
